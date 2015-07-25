d2a075174b1858276aafac1359235c19f943e170
Nirmohi <nirmohi0605@gmail.com>
2015-07-25 14:35:21 -0400

    valid parentheses

--- valid-parentheses-combinations/valid-parentheses-combinations.js
+++ valid-parentheses-combinations/valid-parentheses-combinations.js
@@ -1,4 +1,18 @@
 function validParentheses(n) {
+	var answer = [];
 
+	function getCombinations(open, close, curr) {
+		debugger;
+		if (open === 0 && close === 0)
+			answer.push(curr);
+		if (open > 0) {
+			getCombinations(open - 1, close + 1, curr + '(');
+		}
+
+		if (close > 0) {
+			getCombinations(open, close - 1, curr + ')');
+		}
+	}
+	getCombinations(n, 0, '');
+	return answer;
 }
-
